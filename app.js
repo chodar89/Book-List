065e66ffabe0fd0577672047d52ba6af0af0ac52
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2020-03-01 13:55:31 +0000

    Take book input and append to bottom list

--- app.js
+++ app.js
@@ -8,15 +8,48 @@ function Book(title, author, isbn) {
 // UI Constructor
 function UI() {}
 
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById("book-list");
+
+  // Create tr element
+  const row = document.createElement("tr");
+
+  // Insert cols
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class="delete">X</a></td>
+  `;
+
+  list.appendChild(row);
+};
+
+// Clear fields
+UI.prototype.clearFields = function() {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+
 // Event listeners
 document.getElementById("book-form").addEventListener("submit", function(e) {
   const title = document.getElementById("title").value,
     author = document.getElementById("author").value,
     isbn = document.getElementById("isbn").value;
 
-  //Instantiate book
+  // Instantiate book
   const book = new Book(title, author, isbn);
 
-  console.log(title, author, isbn);
+  // Instantiate UI
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  // Clear fields
+  ui.clearFields();
+
+  console.log(book);
   e.preventDefault();
 });
